df5a9056bdd00e5e5120b21d10f489295d7d530e
EslamAborya <eslam.aborya1251@gmail.com>
2025-02-24 12:00:00 +0200

    Initial commit: Added Angular and Spring Boot projects

---
+++ Frontend/src/app/pages/movies/movies.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieServiceService } from 'src/app/movie-service.service';
+import { Movie } from '../movie';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.scss']
+})
+export class MoviesComponent implements OnInit {
+  movies: Movie[] = [];
+  error: string = '';
+  searchTerm: string = '';
+  currentPage: number = 0;
+  pageSize: number = 10;
+  totalItems: number = 0;
+
+  constructor(private movieService: MovieServiceService) {}
+
+  ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  loadMovies(): void {
+    this.movieService.searchMovies(this.searchTerm, this.currentPage, this.pageSize).subscribe({
+      next: (data: any) => {
+        this.movies = data.content; // List of movies
+        this.totalItems = data.totalElements; // Total number of movies
+      },
+      error: (err) => {
+        console.error('Error loading movies', err);
+        this.error = 'Error loading movies';
+      }
+    });
+  }
+
+  onSearch(): void {
+    this.currentPage = 0; // Reset to the first page
+    this.loadMovies();
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.loadMovies();
+  }
+  totalPages(): number {
+    return Math.ceil(this.totalItems / this.pageSize);
+  }
+}
